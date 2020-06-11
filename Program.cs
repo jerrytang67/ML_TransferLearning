22ed32d892a7b5de278a6b71d28b14b112d47289
TT <tt-tjw@qq.com>
2020-06-11 12:34:32 +0800

    save

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace ML_TransferLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<ImageData>("./labels.csv", separatorChar: ',');
+
+
+            var pipeline = context.Transforms.Conversion.MapValueToKey("LabelKey", "Label")
+                    .Append(context.Transforms.LoadImages("input", "images", nameof(ImageData.ImagePath)))
+                    .Append(context.Transforms.ResizeImages("input", InceptionSettings.ImageWidth, InceptionSettings.ImageHeight, "input"))
+                    .Append(context.Transforms.ExtractPixels("input", interleavePixelColors: InceptionSettings.ChannelsList, offsetImage: InceptionSettings.Mean))
+                    .Append(context.Model.LoadTensorFlowModel("./model/tensorflow_inception_graph.pb")
+                        .ScoreTensorFlowModel(new[]
+                        {
+                            "softmax2_pre_activation"
+                        }, new[]
+                        {
+                            "input"
+                        }, addBatchDimensionInput: true))
+                    .Append(context.MulticlassClassification.Trainers.LbfgsMaximumEntropy("LabelKey", "softmax2_pre_activation"))
+                    .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabelValue", "PredictedLabel"))
+
+                ;
+
+            var model = pipeline.Fit(data);
+
+            var imageData = File.ReadAllLines("./labels.csv")
+                .Select(l => l.Split(','))
+                .Select(l => new ImageData { ImagePath = Path.Combine(Environment.CurrentDirectory, "images", l[0]) });
+
+            var imageDataView = context.Data.LoadFromEnumerable(imageData);
+
+            var predictions = model.Transform(imageDataView);
+
+            var imagePredictions =
+                context.Data.CreateEnumerable<ImagePrediction>(predictions, reuseRowObject: false,
+                    ignoreMissingColumns: true);
+
+            var evalPredictions = model.Transform(data);
+
+            var metrics = context.MulticlassClassification.Evaluate(evalPredictions, labelColumnName: "LabelKey",
+                predictedLabelColumnName: "PredictedLabel");
+
+
+            Console.WriteLine($"Log loss - {metrics.LogLoss}");
+
+
+            var predictionFunc = context.Model.CreatePredictionEngine<ImageData, ImagePrediction>(model);
+
+            var singlePrediction = predictionFunc.Predict((new ImageData()
+            {
+                ImagePath = Path.Combine(Environment.CurrentDirectory, "images", "cup.png")
+            }));
+
+
+            Console.WriteLine($"Image {Path.GetFileName(singlePrediction.ImagePath)} was predited as a  {singlePrediction.PredictedLabelValue} with a score of {singlePrediction.Score.Max()}");
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string ImagePath { get; set; }
+
+        [LoadColumn(1)]
+        public string Label { get; set; }
+    }
+
+    public struct InceptionSettings
+    {
+        public const int ImageHeight = 224;
+        public const int ImageWidth = 224;
+
+        public const float Mean = 117;
+
+        public const float Scale = 1;
+        public const bool ChannelsList = true;
+    }
+
+    public class ImagePrediction : ImageData
+    {
+        public float[] Score { get; set; }
+
+        public string PredictedLabelValue { get; set; }
+    }
+}
